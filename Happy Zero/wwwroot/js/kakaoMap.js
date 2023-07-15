fff74de72438ff9780fbc97bb9e401a21c1c2f5a
JeongWoo Kang <chickenkjw@gmail.com>
2023-07-15 10:16:31 +0900

    지도 api 완료

--- Happy Zero/wwwroot/js/kakaoMap.js
+++ Happy Zero/wwwroot/js/kakaoMap.js
@@ -1,36 +1,33 @@
-﻿function setMapMaker() {
+﻿var mapContainer = document.getElementById('map'), // 지도를 표시할 div  
+    mapOption = {
+        center: new kakao.maps.LatLng(37.44931, 126.65437), // 지도의 중심좌표
+        level: 6 // 지도의 확대 레벨
+    };
+
+var map = new kakao.maps.Map(mapContainer, mapOption);
+
+function getLatlng(lat, lng) {
+    return new kakao.maps.LatLng(lat, lng);
+}
+
+function setMapMaker(curLat, curLng, _positions) {
+    var positions = JSON.parse(_positions);
+
     var mapContainer = document.getElementById('map'), // 지도를 표시할 div  
         mapOption = {
-            center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+            center: new kakao.maps.LatLng(curLat, curLng), // 지도의 중심좌표
             level: 6 // 지도의 확대 레벨
         };
 
     var map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
 
-    // 마커를 표시할 위치와 title 객체 배열입니다 
-    var positions = [
-        {
-            title: '카카오',
-            latlng: new kakao.maps.LatLng(33.450705, 126.570677)
-        },
-        {
-            title: '생태연못',
-            latlng: new kakao.maps.LatLng(33.450936, 126.569477)
-        },
-        {
-            title: '텃밭',
-            latlng: new kakao.maps.LatLng(33.450879, 126.569940)
-        },
-        {
-            title: '근린공원',
-            latlng: new kakao.maps.LatLng(33.451393, 126.570738)
-        }
-    ];
+    // 마커를 표시할 위치와 title 객체 배열입니다
 
     // 마커 이미지의 이미지 주소입니다
     var imageSrc = "https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/markerStar.png";
 
     for (var i = 0; i < positions.length; i++) {
+        console.log(positions[i]);
 
         // 마커 이미지의 이미지 크기 입니다
         var imageSize = new kakao.maps.Size(24, 35);
@@ -41,7 +38,8 @@
         // 마커를 생성합니다
         var marker = new kakao.maps.Marker({
             map: map, // 마커를 표시할 지도
-            position: positions[i].latlng, // 마커를 표시할 위치
+            //position: positions[i].latlng, // 마커를 표시할 위치
+            position: getLatlng(positions[i].lat, positions[i].lng), // 마커를 표시할 위치
             title: positions[i].title, // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
             image: markerImage // 마커 이미지 
         });
